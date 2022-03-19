832514304575c8359108df45f9071c1b36e262fa
qqudra <48051988+qqudra@users.noreply.github.com>
2022-03-19 19:26:20 +0300

    Add files via upload
    Добавил поддержку мультидропдаунов

--- main.py
+++ main.py
@@ -3,12 +3,8 @@
 
 # #Сюда подставляете свой токен
 
-from ast import Div
-import pandas as pd
-from dash import Dash, html, dcc,Input,Output,State
+from dash import Dash, html, dcc,Input,Output
 import meteofunc
-import plotly.graph_objs as go
-import numpy as np
 
 
 field = 't2m'
@@ -21,6 +17,18 @@ region = '125_ETR+'
 stat = 'rmse_05'
 source = 'Blinov'
 
+stat1 = []
+model1 = []
+field1 = []
+region1 = []
+term1 = []
+
+stat_c = 0
+model_c = 0
+field_c = 0
+region_c = 0
+term_c = 0
+
 field_m,stat_m,model_m,basePeriod_m, region_m, term_m = meteofunc.create_data()
 
 
@@ -29,56 +37,106 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div([
 
-        html.Div(children=[
-            html.Label('Stat'),
-            dcc.Dropdown(stat_m,  id='stat_id')]),
+        html.Button('Submit', id='submit-val', n_clicks=0),
+        html.Div(id='container-button-basic',
+             children='Enter a value and press submit'),
 
         html.Div(children=[
+            html.Label('Stat'),
+            dcc.Dropdown(stat_m,  id='stat_id',multi = True)]),
+            html.Div(id='dd-output-container'),
+        
+        html.Div(children=[
             html.Label('Model'),
-            dcc.Dropdown(model_m,  id='model_id')]),
+            dcc.Dropdown(model_m,  id='model_id',multi = True)]),
+            html.Div(id='dd-output-container1'),
 
         html.Div(children=[
             html.Label('Field'),
-            dcc.Dropdown(field_m,  id='field_id')]),
+            dcc.Dropdown(field_m,  id='field_id',multi = True)]),
+            html.Div(id='dd-output-container2'),
 
         html.Div(children=[
             html.Label('Region'),
-            dcc.Dropdown(region_m, id='region_id')]),
+            dcc.Dropdown(region_m, id='region_id',multi = True)]),
+            html.Div(id='dd-output-container3'),
+
 
         html.Div(children=[
             html.Label('Term'),
-            dcc.Dropdown(term_m, id='term_id')]),
+            dcc.Dropdown(term_m, id='term_id',multi = True)]),
+            html.Div(id='dd-output-container4'),
+
 
         html.Div([ dcc.Graph(id = "graph"),
-            #    dcc.Slider(min = 0, max = 48, step = 3, id = "sliders",
-            #    value = 48)
 
     ], style={'padding': 10, 'flex': 1}),    
     ],style={'width': '100%', 'display': 'inline-block'}),
 
 ], style={'display': 'flex'})
 
+
+@app.callback(
+    Output('dd-output-container', 'children'),
+    Input('stat_id', 'value'),
+)
+def set_stat(stat_id):
+    global stat_c
+    stat1.append(stat_id[stat_c])
+    stat_c+=1
+
+@app.callback(
+    Output('dd-output-container1', 'children'),
+    Input('model_id', 'value'),
+)
+def set_model(model_id):
+    global model_c
+    model1.append(model_id[model_c])
+    model_c+=1
+@app.callback(
+    Output('dd-output-container2', 'children'),
+    Input('field_id', 'value'),
+)
+def set_field(field_id):
+    global field_c
+    field1.append(field_id[field_c])
+    field_c+=1
+
+@app.callback(
+    Output('dd-output-container3', 'children'),
+    Input('region_id', 'value'),
+)
+def set_region(region_id):
+    global region_c
+    region1.append(region_id[region_c])
+    region_c+=1
+
+@app.callback(
+    Output('dd-output-container4', 'children'),
+    Input('term_id', 'value'),
+)
+def set_term(term_id):
+    global term_c
+    term1.append(term_id[term_c])
+    term_c+=1
+
 @app.callback(
     Output('graph', 'figure'),
-    Input('stat_id', 'value'),
-    Input('model_id', 'value'),
-    Input('field_id', 'value'),
-    Input('region_id', 'value'),
-    Input('term_id', 'value'),
-    # Input('sliders', 'value'),
+    Input('submit-val', 'n_clicks'),
 )
-def g1(stat_id,model_id,field_id,region_id,term_id):
-    global stat,model,field,region,term
-    
-    stat = stat_id
-    model = model_id
-    field = field_id
-    region = region_id
-    term = term_id
 
-    print(field,model,exp,basePeriod,time,term,region,stat,source)
-    fig = meteofunc.read_data(field,model,exp,basePeriod,time,term,region,stat,source)
-    return fig  
+def bb(n_clicks):
+    global stat1,field1,model1,region1,term1
+
+    # print(stat1)
+    # print(field1)
+    # print(model1)
+    # print(region1)
+    # print(term1)
+
+    fig = meteofunc.read_data(field1,model1,exp,basePeriod,time,term1,region1,stat1,source)
+    return fig
 
 if __name__ == '__main__':
+    # app.run_server(host= '0.0.0.0',debug=False)
     app.run_server(debug=True)
--- meteofunc.py
+++ meteofunc.py
@@ -33,7 +33,7 @@ def create_data():
     ###############################Region####################################
     region_m = sort_field(DATA,"region")
 
-    ###############################Tern####################################
+    ###############################Term######################################
     term_m = sort_field(DATA,"term")
     print(term_m)
 
@@ -42,16 +42,29 @@ def create_data():
 def read_data(field,model,exp,basePeriod,time,term,region,stat,source):
 
     data1 = [[]]
-    for i in range (len(DATA)):
-        # if DATA['stat'][i] == (stat) and DATA['model'][i] == model and DATA['field'][i] == field and DATA['basePeriod'][i] == basePeriod and DATA['timeperiod'][i] == time and DATA['term'][i] == term and DATA['region'][i] == region and DATA['source'][i] == source and DATA['exp'][i] == exp:
-        if DATA['stat'][i] == stat and DATA['model'][i] == model and DATA['field'][i] == field and DATA['basePeriod'][i] == basePeriod and DATA['term'][i] == term and DATA['region'][i] == region:
+
+    i = 0
+
+    # for i in range (len(DATA)):
+    #     # for j in range(len(stat) ):
+
+    #         if DATA['stat'][i] == stat and DATA['model'][i] == model and DATA['field'][i] == field and DATA['basePeriod'][i] == basePeriod and DATA['term'][i] == term and DATA['region'][i] == region:
+    #             data1.append([DATA['00'][i],DATA['03'][i],DATA['06'][i],DATA['09'][i],
+    #             DATA['12'][i],DATA['15'][i],DATA['18'][i],DATA['21'][i],DATA['24'][i],
+    #             DATA['27'][i],DATA['30'][i],DATA['33'][i],DATA['36'][i],DATA['39'][i],
+    #             DATA['42'][i],DATA['45'][i],DATA['48'][i]])
+    # data1.pop(0)
+
+    print("Go")
+    for i in range(len(DATA)):
+        if (DATA['stat'][i] in stat) and (DATA['model'][i] in model) and (DATA['field'][i] in field) and (DATA['basePeriod'][i] in basePeriod) and (DATA['term'][i] in term) and (DATA['region'][i] in region):
             data1.append([DATA['00'][i],DATA['03'][i],DATA['06'][i],DATA['09'][i],
             DATA['12'][i],DATA['15'][i],DATA['18'][i],DATA['21'][i],DATA['24'][i],
             DATA['27'][i],DATA['30'][i],DATA['33'][i],DATA['36'][i],DATA['39'][i],
             DATA['42'][i],DATA['45'][i],DATA['48'][i]])
+            print("done")
     data1.pop(0)
 
-    print(data1)
     fig = go.Figure()
 
     for i in range (len(data1)):
